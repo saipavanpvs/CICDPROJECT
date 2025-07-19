7b977c7c69132dd89bdd29c3e7fca4d011f18e6b
pothuri.saipavan3 <pothuri.saipavan3@gmail.com>
2025-07-19 12:18:47 +0530

    message

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,13 +6,13 @@ data "aws_vpc" "default" {
     default = true
 }
 
-resource "aws_securrity_group" "My_security_group" {
+resource "aws_security_group" "My_security_group" {
     name = "Primary_security_group"
     vpc_id= data.aws_vpc.default.id
 
     ingress {
         description = "Allow SSH"
-        from _port = 22
+        from_port = 22
         to_port =22
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -20,7 +20,7 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "Allow SMTP"
-        from _port = 25
+        from_port = 25
         to_port =25
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -28,7 +28,7 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "Custom TCP"
-        from _port = 3000
+        from_port = 3000
         to_port =10000
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -36,7 +36,7 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "Allow Http"
-        from _port = 80
+        from_port = 80
         to_port =80
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -44,7 +44,7 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "Allow HTTPS"
-        from _port = 443
+        from_port = 443
         to_port =443
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -52,7 +52,7 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "custom TCP for Kubernates"
-        from _port = 6443
+        from_port = 6443
         to_port =6443
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -60,7 +60,7 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "Mail Notification"
-        from _port = 465
+        from_port = 465
         to_port =465
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
@@ -68,14 +68,14 @@ resource "aws_securrity_group" "My_security_group" {
 
     ingress {
         description = "Nodeport"
-        from _port = 30000
+        from_port = 30000
         to_port =32767
         protocol ="tcp"
         cidr_blocks =["0.0.0.0/0"]
     }
 
-    engress {
-        from _port = 0
+    egress {
+        from_port = 0
         to_port =0
         protocol ="-1"
         cidr_blocks =["0.0.0.0/0"]
